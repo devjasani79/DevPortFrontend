3bb4bbd1292a01cccf260a7597d15244b6070b0f
Dev Jasani <devjasani79@gmail.com>
2025-08-19 18:58:01 +0530

    demo

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,4 @@
-import Navbar from "../components/layout/navbar";
+import Navbar from "../components/layout/Navbar";
 import Footer from "../components/layout/Footer";
 import Hero from "../components/landing/Hero";
 import Features from "../components/landing/Features";
